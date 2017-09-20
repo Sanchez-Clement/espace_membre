8c05dd3428ac361a5f5e7bb8e3f36472ac5370e1
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-20 10:31:49 +0200

    make the connexion in connexion_sql.php

---
+++ modele/connexion_sql.php
@@ -0,0 +1,8 @@
+<!-- Connexion base de données -->
+
+<?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=espaceMembre;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (Exception $e) {
+    die('Erreur : '.$e->getMessage());
+}
