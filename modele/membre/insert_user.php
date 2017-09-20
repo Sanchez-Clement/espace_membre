93e0399679d59ede53293110a506da9a68901ab9
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-20 11:00:28 +0200

    make the function insert_user and insert_avatar in inser_user.php

---
+++ modele/membre/insert_user.php
@@ -0,0 +1,26 @@
+<?php
+
+function insert_user($pseudo,$passw,$mail,$signature) {
+  global $bdd;
+  $req = $bdd->prepare('INSERT INTO membre (pseudo, passw, mail, signature,date_inscription) VALUES(:pseudo, :passw, :mail, :signature, CURDATE())');
+  $req->execute(array(
+  'pseudo' => htmlspecialchars($pseudo),
+  'passw' => htmlspecialchars($passw),
+  'mail' => htmlspecialchars($mail) ,
+  'signature' => htmlspecialchars($signature),
+
+
+  ));
+
+}
+
+function insert_avatar($nom_image,$source) {
+  global $bdd;
+  $req = $bdd->prepare('INSERT INTO avatar (id_membre, nom_image, source) VALUES(:id_membre, :nom_image, :source)');
+  $req->execute(array(
+  'id_membre' => $bdd -> lastInsertId(),
+  'nom_image' => htmlspecialchars($nom_image),
+  'source' => htmlspecialchars($source) ,
+
+));
+}
